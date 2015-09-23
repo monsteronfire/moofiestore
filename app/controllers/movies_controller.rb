3ff45d3f94011cbf80d3039b17764016f04bd37b
monsteronfire <monsteronfire@gmail.com>
2015-09-23 12:18:02 +0800

    configures redis and adds cart controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,18 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @movies = Movie.all.order("created_at DESC")
   end
 
   def show
+    @cart_action = @movie.cart_action current_user.try :id
+  end
+
+  private
+
+  def find_movie
+    @movie = Movie.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+
   resources :movies, only: [:show, :index]
+  
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
   
   devise_for :users
 
